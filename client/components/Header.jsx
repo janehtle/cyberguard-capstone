47ad05f477f0457ed7d74463027ee8cba1950f4d
Jane Le <janehtle@gmail.com>
2025-12-01 12:46:13 -0500

    quiz homepage created and coded, minor touch ups to header/footer/index.css

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../styles/header.css";
-import Placeholder from "../assets/mascot.png";
+import Logo from "../assets/logo-removebg.png";
 import { Link } from "react-router-dom";
 
 
 export default function Header() {
     return (
         <header>
-            <Link to="/"><img src={Placeholder} className="placeholder" alt="green bean" /></Link>
+            <Link to="/"><img src={Logo} className="logo" alt="cyberguard logo" /></Link>
 
             <ul className="nav-links">
                 <li>
